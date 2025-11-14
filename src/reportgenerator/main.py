f8ee15dc0584334791ec91fe6bf2f6cbc3099a71
Vamsi-Krishna-B <boggarapuvamsikrishna@gmail.com>
2025-11-14 10:42:24 +0530

    Final Pushh

--- src/reportgenerator/main.py
+++ src/reportgenerator/main.py
@@ -7,7 +7,7 @@ class ReportGeneratorAPP:
         
         self.graph = Build_Graph().compile()
         self.final_report = '' 
-    
+
     def save_as_md(self,file_path):
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(self.final_report)
@@ -16,7 +16,6 @@ class ReportGeneratorAPP:
         display(Image(self.graph.get_graph().draw_mermaid_png()))
 
     def generate_report(self, isUserInput, user_input=None,topic=None):
-        final_report = ""
         if isUserInput:
             result = self.graph.invoke(
                 {"is_userInput": isUserInput, "user": {"user_input": user_input}}
